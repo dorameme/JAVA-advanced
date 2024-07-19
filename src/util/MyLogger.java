560b1318ac7271ab1d751389db4aa60ba32af4b8
dorameme <doradorav@naver.com>
2024-07-19 13:29:50 +0900

    여러 스레드 만들기

--- src/util/MyLogger.java
+++ src/util/MyLogger.java
@@ -7,6 +7,6 @@ public class MyLogger {
  public static final DateTimeFormatter formatter= DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
  public static void log(Object obj){
      String time= LocalTime.now().format(formatter);
-     System.out.printf("%s [%9s] %s",time,Thread.currentThread().getName(),obj);
+     System.out.printf("%s [%9s] %s \n",time,Thread.currentThread().getName(),obj);
  }
 }
